1f37c1a8eedfbbc5e9913637d52ccd321b896f8d
jubecerrap <jubecerrap@unal.edu.co>
2023-11-12 11:14:42 -0500

    Pantalla de inicio

---
+++ Levels/titleScreen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/base.tscn")
