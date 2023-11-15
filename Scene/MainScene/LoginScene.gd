145fa60d8dc6dba5baa24bb68ffdc59438f1c951
Axel Gomez <axgomezm@unal.edu.co>
2023-11-15 12:33:20 -0500

    cambios en el script

--- Scene/MainScene/LoginScene.gd
+++ Scene/MainScene/LoginScene.gd
@@ -9,8 +9,7 @@ func _ready():
 	username_line = $TextureRect/NinePatchRect/VBoxContainer/LineEdit
 	password_line = $TextureRect/NinePatchRect/VBoxContainer/LineEdit2
 	message_label = $TextureRect/NinePatchRect/VBoxContainer/Message
-	message_label.visible = false  
-	password_line.secret = true
+	
 func _on_login_button_button_down():
 	var username = username_line.text.strip_edges()
 	var password = password_line.text.strip_edges()
@@ -18,13 +17,13 @@ func _on_login_button_button_down():
 
 func check_and_register(username, password):	
 	if username == "" or password =="" :
-		show_message("fill the blank spaces")
+		show_message("fill the blank spaces",)
 		return
 	message_label.visible = false
-		
+
 func show_message(message):
 	message_label.text = message
 	message_label.visible = true
-		
+
 func _on_singup_button_button_down():
 	get_tree().change_scene_to_file(singup_screen_path)
--- Scene/MainScene/singupScene.gd
+++ Scene/MainScene/singupScene.gd
@@ -6,17 +6,14 @@ var username_line
 var email_line 
 var password_line
 var message_label
+var message2_label
 
 func _ready():
 	username_line = $TextureRect/NinePatchRect/VBoxContainer/LineEdit
 	email_line = $TextureRect/NinePatchRect/VBoxContainer/LineEdit2
 	password_line = $TextureRect/NinePatchRect/VBoxContainer/LineEdit3
 	message_label = $TextureRect/NinePatchRect/VBoxContainer/Message
-	password_line.secret = true
-	message_label.visible = false  
-
-func _on_login_button_button_down():
-	get_tree().change_scene_to_file(login_screen_path)
+	message2_label = $TextureRect/NinePatchRect/VBoxContainer/Message2  
 
 func _on_create_button_button_down():
 	var username = username_line.text.strip_edges()
@@ -26,58 +23,43 @@ func _on_create_button_button_down():
 	
 #valida y registra
 func check_and_register(username, email, password):	
+	
 	if username == "" or password =="" or email =="":
-		show_message("fill the blank spaces")
+		show_message("fill the blank spaces","red")
 		return
 	if len(username)<4:
-		show_message("username too short")
+		show_message("username too short","red")
 		return
 	if len(password)<4:
-		show_message("password too short")
+		show_message("password too short","red")
 		return
 	if is_valid_email(email):
-		show_message("Invalid email format")
+		show_message("Invalid email format","red")
 		return	
-	message_label.visible = false
-	get_tree().change_scene_to_file(login_screen_path)
-# Muestra el mensaje de error 
-func show_message(message):
-	message_label.text = message
-	message_label.visible = true
-	
+		
+	show_message("User created","green")
+
 #validar email	
 func is_valid_email(email):
 	var regex = RegEx.new()
 	regex.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
 	return regex.search(email) == null
 	
-#	var json_data = load_json(file_path)
-## Verificar si el username ya existe
-#	if user_exist(username, json_data):
-#		print("Username alredy exist")
-#	else:
-#		# Agregar el nuevo usuario al JSON
-#		json_data.append({"username":username, "password": password, "email": email})
-#		save_json(file_path,json_data)
-#		print("Usuario registrado correctamente.")
+# Muestra el mensaje de error o creacion de perfil completada
+func show_message(message, color):
+	if color == "green":
+		message_label.visible = false
+		message2_label.text = message
+		message2_label.visible = true
+		username_line.text = ""
+		email_line.text = ""
+		password_line.text = ""
+		
+	if color == "red":
+		message2_label.visible = false
+		message_label.text = message
+		message_label.visible = true
+		
 
-#func user_exist(username, data):
-#	for user in data:
-#		print(user.username, username)
-#		if user.username == username:
-#			return true
-#	return false
-#
-#func load_json(file_path):
-#	if FileAccess.file_exists(file_path):
-#		var dataFile = FileAccess.open(file_path, FileAccess.READ)
-#		var parsedResult = JSON.parse_string(dataFile.get_as_text())
-#		return parsedResult
-#	else:
-#		print("File doesnt exist")
-#
-#func save_json(file_path, new_data):
-#	if FileAccess.file_exists(file_path):
-#		var json_data = new_data.to_json()
-#		var data_file = FileAccess.open(file_path, FileAccess.WRITE)
-#		data_file.
+func _on_login_button_button_down():
+	get_tree().change_scene_to_file(login_screen_path)
