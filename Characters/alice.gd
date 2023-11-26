3520c4d17b0cff596099597a7133002482d95f46
jubecerrap <jubecerrap@unal.edu.co>
2023-11-26 13:04:38 -0500

    Dialogo introductorio

--- Characters/alice.gd
+++ Characters/alice.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
 signal item_droped(item: Area2D)
-
 var speed:int = 80
 
 @onready var ap = $AnimationPlayer
@@ -11,12 +10,17 @@ var speed:int = 80
 var current_inventory: Array
 var past_direction: Vector2 = Vector2.ZERO
 var can_collect: bool = true
+var on_door: bool = false
+var on_serpent: bool = false
+var first_interaction: bool = true
+var snake_position : Vector2 = Vector2(120,23) 
 
 func _ready():
 	inventory.droped.connect(drop_item)
 
 func _process(_delta):
 	
+	dialogue()
 	# input
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction.normalized() * speed
@@ -26,6 +30,7 @@ func _process(_delta):
 		sprite.flip_h = (direction.x >= -1 and direction.x < 0)
 	
 	move_and_slide()
+	
 	
 	if past_direction != direction:
 		update_animations(direction)
@@ -91,7 +96,7 @@ func _on_area_2d_area_entered(area):
 		if area not in current_inventory:
 			current_inventory.append(area.duplicate())
 			area.collect(inventory)
-
+	
 func print_inventory():
 	for i in current_inventory:
 		print(i)
@@ -107,7 +112,40 @@ func drop_item(item: InventoryItem):
 			item_droped.emit(current_inventory[i])
 			pos = i
 	current_inventory.erase(current_inventory[pos])
+	
+func dialogue():
+		
+	if on_door and first_interaction == false and Input.is_action_pressed("accept"):
+		DialogueManager.show_dialogue_balloon(load("res://Dialogues/main.dialogue"),"discussion")
+		
+	elif Input.is_action_pressed("accept") and (on_door or on_serpent) :
+		if $"../Snake".position != snake_position:
+			$"../Snake".position =  snake_position
+			$"../Snake02".position = snake_position
+		ap.play("idle")
+		first_interaction = false
+		DialogueManager.show_dialogue_balloon(load("res://Dialogues/main.dialogue"),"start")	
+
 
 
 func _on_collect_timer_timeout():
 	can_collect = true
+
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("openDoor"):
+		on_door = true
+		print("ondoor")
+		
+	elif body.has_method("onSerpent"):
+		on_serpent = true
+		print("onSerpent")
+		
+func _on_area_2d_body_exited(body):
+	if body.has_method("openDoor"):
+		on_door = false
+		print("exiteDoor")
+		
+	elif body.has_method("onSerpent"):
+		on_serpent = false
+		print("exitSerpent")
---
+++ Objects/Door.gd
@@ -0,0 +1,5 @@
+extends StaticBody2D
+
+# Called when the node enters the scene tree for the first tim
+func openDoor():
+	pass
