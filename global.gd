6b31632f34dc0317e8966dbc4c79f8cdc9c4d3ad
Diego Mellizo <dmellizo@unal.edu.co>
2023-11-26 13:12:19 -0500

    Final map

--- global.gd
+++ global.gd
@@ -2,6 +2,7 @@ extends Node
 
 var matrix 
 var matrix_size = 7
+var rooms_alredy_passed: Array
 var current_point
 var end_point
 var current_scene = "base"
@@ -15,6 +16,7 @@ func finish_changedscenes():
 
 func change_room():
 	current_scene = "level1"
+	rooms_alredy_passed.append(matrix[current_point.x][current_point.y]["id"])
 	get_tree().change_scene_to_file(matrix[current_point.x][current_point.y]["type"])
 
 func next_room(direction):
