864898d4c67d4b5bc2d303ea7aeba5e621e4acf9
Esteban <luleonr@unal.edu.co>
2023-11-28 00:34:52 -0500

    Conexiones

--- Scene/MainScene/MainMenu.gd
+++ Scene/MainScene/MainMenu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+var settings_screen_path ="res://Scene/MainScene/MainMenu_settings.tscn"
+var statistics_screen_path ="res://Scene/MainScene/MainMenu_statistics.tscn"
+var Play_screen_path ="res://Scene/MainScene/PlayMenu.tscn"
+@onready var settings = $Exit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +13,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_ajustes_pressed():
+	get_tree().change_scene_to_file(settings_screen_path)
+
+
+func _on_estadisticas_pressed():
+	get_tree().change_scene_to_file(statistics_screen_path)
+
+
+func _on_jugar_pressed():
+	get_tree().change_scene_to_file(Play_screen_path)
+
+
+func _on_cerrar_sesion_pressed():
+	settings.sett_open()
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scene/MainScene/MainMenu.tscn")
